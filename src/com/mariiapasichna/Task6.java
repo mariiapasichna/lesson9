5fc7215a45892ad479378932188d2d2a38fdf9f1
Mariia Pasichna <pasichnayam@gmail.com>
2020-01-18 14:49:12 +0200

    correct

--- src/com/mariiapasichna/Task6.java
+++ src/com/mariiapasichna/Task6.java
@@ -9,13 +9,15 @@ public class Task6 {
 
     public static void main(String[] args) {
         int count = 0;
-        for (int a = 1; a < 100; a++) {
-            for (int b = 1; b < 100; b++) {
-                for (int c = 1; c < 100; c++) {
-                    int p = (a + b + c) / 2;
-                    double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-                    if (S <= 100) {
-                        count++;
+        for (int a = 1; a <= 200; a++) {
+            for (int b = a; b <= 200; b++) {
+                for (int c = b; c <= 200; c++) {
+                    if (a + b > c) {
+                        double p = (a + b + c) / 2.0;
+                        double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+                        if (S <= 100) {
+                            count++;
+                        }
                     }
                 }
             }
--- src/com/mariiapasichna/Task_5.java
+++ src/com/mariiapasichna/Task_5.java
@@ -2,7 +2,7 @@ package com.mariiapasichna;
 
 import java.util.Arrays;
 
-public class Task5 {
+public class Task_5 {
 
     /*5*) Написать метод который ищет при помощи бинарного поиска есть ли число в массиве.
 Параметры - отсортированный массив и число которое надо проверить. Результат типа boolean.*/
@@ -12,24 +12,25 @@ public class Task5 {
     public static void main(String[] args) {
         int arr[] = new int[SIZE];
         int number = 78;
-        int first = 0;
-        int last = arr.length - 1;
         Task1.createArrayAndInit(arr);
+        //System.out.println(Arrays.toString(arr));
         Arrays.sort(arr);
-        binarySearch(arr, number, first, last);
+        binarySearch(arr, number);
     }
 
-    private static boolean binarySearch(int[] arr, int number, int first, int last) {
-        if (first <= last) {
+    private static boolean binarySearch(int[] arr, int number) {
+        int first = 0;
+        int last = arr.length - 1;
+
+        while (first <= last) {
             int middle = (first + last) / 2;
             if (arr[middle] == number) {
                 System.out.println("The number " + number + " is found by index " + middle);
                 return true;
-            }
-            if (arr[middle] > number) {
-                return binarySearch(arr, number, first, middle - 1);
-            } else {
-                return binarySearch(arr, number, middle + 1, last);
+            } else if (arr[middle] < number) {
+                first = middle + 1;
+            } else if (arr[middle] > number) {
+                last = middle - 1;
             }
         }
         System.out.println("The number " + number + " isn't found");
